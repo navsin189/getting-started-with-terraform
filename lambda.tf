1b332db0076eb3120a86c64f013043b41f46a0e3
Naveen Singh <naveen.singh@mindfiresolutions.com>
2023-09-27 11:58:00 +0530

    introduce AWS lambda tf file

---
+++ lambda.tf
@@ -0,0 +1,31 @@
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_logging.json
+}
+
+data "archive_file" "lambda_file" {
+  type        = "zip"
+  source_file = "lambda.js"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  function_name = "DemoLambda"
+  filename      = "lambda_function_payload.zip"
+  role          = aws_iam_role.iam_for_lambda.arn
+  runtime       = "nodejs18.x"
+  handler       = "lambda.js"
+}
